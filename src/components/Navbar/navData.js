5ea5a73708bb0d8365c81b09dfb7db6bb1d8af09
Aryan Bains <aryanbains6@gmail.com>
2024-05-24 09:40:53 +0530

    Bug Fixes

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,39 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  // {
+  //   label: 'Contact',
+  //   pathname: '/contact',
+  // },
+  {
+    label: 'Photography',
+    pathname: 'https://photography.tomasps.com',
+  },
+  {
+    label: 'Drive',
+    pathname: 'https://drive.tomasps.com',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/toomas_ps',
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/Tresillo2017',
+    icon: 'github',
+  },
+];
---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,161 @@
+import gamestackTexture2Large from 'assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from 'assets/gamestack-list.jpg';
+import gamestackTextureLarge from 'assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from 'assets/gamestack-login.jpg';
+import sliceTextureLarge from 'assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
+import sliceTexture from 'assets/slice-app.jpg';
+import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { ProjectSummary } from 'layouts/Home/ProjectSummary';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+import { Helmet } from 'react-helmet';
+
+const disciplines = ['Developer', 'Photographer', 'Helper'];
+
+
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+  
+  
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Helmet>
+      <script defer data-domain="tomasps.com" src="https://plausible.tomasps.com/js/script.js"></script>
+    </Helmet>
+      <Meta
+        title="Designer + Developer"
+        description="Design portfolio of TomasPS — a product designer working on web & mobile
+          apps with a focus on motion, experience design, and accessibility."
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Tesla-Dashboard"
+        description="Tesla dashboard for beamng drive"
+        buttonText="View more"
+        buttonLink="/projects/tesla-dashboard"
+        model={{
+          type: 'laptop',
+          alt: 'Screenshot of the current UI',
+          textures: [
+            {
+              srcSet: [sprTexture, sprTextureLarge],
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="SpainRP"
+        description="SpainRP Minecraft Server"
+        buttonText="View website"
+        buttonLink="https://spainrp.me"
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="Vectorify"
+        description="A vector drawing app made with C# and WinUI3"
+        buttonText="View website"
+        buttonLink="https://github.com/tresillo2017/vectorify"
+        model={{
+          type: 'laptop',
+          alt: 'A screenshot of the current UI',
+          textures: [
+            {
+              srcSet: [sliceTexture, sliceTextureLarge],
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
